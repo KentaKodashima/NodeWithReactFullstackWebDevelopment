e104da50a8e51491690f927a09a612caa561c933
Kenta Kodashima <kentakodashima@gmail.com>
2019-04-01 00:14:24 -0700

    Add react-spring to implement animations on the Dashboard.

--- server/client/src/components/surveys/SurveyList.js
+++ server/client/src/components/surveys/SurveyList.js
@@ -2,7 +2,8 @@ import './SurveyList.scss'
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import { fetchSurveys, deleteSurvey } from '../../actions'
-import {Link} from "react-router-dom"
+import {Link} from 'react-router-dom'
+import { Transition } from 'react-spring/renderprops'
 
 import RequireLogin from './RequireLogin'
 
@@ -31,7 +32,7 @@ class SurveyList extends Component{
   }
 
   renderSurveys() {
-    if (!this.props.auth) {
+    if (this.props.auth === false) {
       return <RequireLogin />
     }
 
@@ -39,38 +40,78 @@ class SurveyList extends Component{
       return this.renderNoSurveysScreen()
     }
 
-    return this.props.surveys.reverse().map(survey => {
-      return (
-        <div className="col s12 m6 l6" key={survey._id} >
-          <div className="card grey lighten-4">
-            <div className="card-content" >
-              <span className="card-title deep-purple-text text-lighten-1">{survey.title}</span>
-              <div className="card-body">
-                {survey.body}
+    return (
+      <Transition
+        items={this.props.surveys.reverse()} keys={ survey => survey._id }
+        from={{ transform: 'translateX(20px)' }}
+        enter={{ transform: 'translateX(0px)' }}
+        leave={{ opacity: 0 }}>
+        { survey => props => (
+          <div style={props} className="col s12 m6 l6" >
+            <div className="card grey lighten-4">
+              <div className="card-content" >
+                <span className="card-title deep-purple-text text-lighten-1">{survey.title}</span>
+                <div className="card-body">
+                  {survey.body}
+                </div>
+                <div>
+                  <p>
+                    Sent On: { new Date(survey.dateSent).toLocaleDateString() }
+                  </p>
+                  <p>
+                    Last Responded: { this.renderLastResponded(survey) }
+                  </p>
+                </div>
               </div>
-              <div>
-                <p>
-                  Sent On: { new Date(survey.dateSent).toLocaleDateString() }
-                </p>
-                <p>
-                  Last Responded: { this.renderLastResponded(survey) }
-                </p>
+              <div className="card-action">
+                <a className="deep-purple-text text-lighten-1">Yes: {survey.yes}</a>
+                <a className="deep-purple-text text-lighten-1">No: {survey.no}</a>
+                <button
+                  className="btn-floating btn-small waves-effect waves-light right"
+                  onClick={() => this.props.deleteSurvey(survey._id)}
+                >
+                  <i className="material-icons deep-purple lighten-1">delete_forever</i>
+                </button>
               </div>
             </div>
-            <div className="card-action">
-              <a className="deep-purple-text text-lighten-1">Yes: {survey.yes}</a>
-              <a className="deep-purple-text text-lighten-1">No: {survey.no}</a>
-              <button
-                className="btn-floating btn-small waves-effect waves-light right"
-                onClick={() => this.props.deleteSurvey(survey._id)}
-              >
-                <i className="material-icons deep-purple lighten-1">delete_forever</i>
-              </button>
-            </div>
-        </div>
-        </div>
-      )
-    })
+          </div>
+        )}
+        {/*<div style={props}>{survey.body}</div> }*/}
+      </Transition>
+    )
+
+    // return this.props.surveys.reverse().map(survey => {
+    //   return (
+    //     <div className="col s12 m6 l6" key={survey._id} >
+    //       <div className="card grey lighten-4">
+    //         <div className="card-content" >
+    //           <span className="card-title deep-purple-text text-lighten-1">{survey.title}</span>
+    //           <div className="card-body">
+    //             {survey.body}
+    //           </div>
+    //           <div>
+    //             <p>
+    //               Sent On: { new Date(survey.dateSent).toLocaleDateString() }
+    //             </p>
+    //             <p>
+    //               Last Responded: { this.renderLastResponded(survey) }
+    //             </p>
+    //           </div>
+    //         </div>
+    //         <div className="card-action">
+    //           <a className="deep-purple-text text-lighten-1">Yes: {survey.yes}</a>
+    //           <a className="deep-purple-text text-lighten-1">No: {survey.no}</a>
+    //           <button
+    //             className="btn-floating btn-small waves-effect waves-light right"
+    //             onClick={() => this.props.deleteSurvey(survey._id)}
+    //           >
+    //             <i className="material-icons deep-purple lighten-1">delete_forever</i>
+    //           </button>
+    //         </div>
+    //     </div>
+    //     </div>
+    //   )
+    // })
   }
 
   render() {
--- server/client/src/utils/validateEmail.js
+++ server/client/src/utils/validateEmail.js
@@ -7,7 +7,7 @@ export default (emails) => {
     .filter(email => re.test(email) === false)
 
   if (invalidEmails.length) {
-    return `These emails are invalid: ${invalidEmails}`
+    return `These emails are invalid: ${invalidEmails}. Enter the correct emails and use ',' to separate them.`
   }
 
   return
